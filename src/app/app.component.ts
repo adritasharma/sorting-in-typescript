e995cac6de165c0e14abe91ef267bf6c3c4e9764
adritasharma <adritasharma.developer@gmail.com>
2020-05-06 00:30:58 +0530

    bubble-sort-complexity added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  items = [{ display: "bubble-sort", path: "bubble-sort" }];
+  items = [{ display: "Bubble Sort", path: "bubble-sort" }];
 }
--- src/app/bubble-sort/bubble-sort.component.ts
+++ src/app/bubble-sort/bubble-sort.component.ts
@@ -14,7 +14,7 @@ export class BubbleSortComponent implements OnInit {
 
   ngOnInit() {
     let length = this.output.length;
-    let arr = this.output
+    let arr = this.output;
 
     for (var i = 0; i < length - 1; i++) {
       for (var j = 0; j < length - i - 1; j++) {
@@ -35,12 +35,19 @@ export class BubbleSortComponent implements OnInit {
         }
       }
     }
-  `
+  `;
 
   notes = [
     `Outer loop is responsible to get the biggest element to the last.`,
     `The inner loop is responsible to swap unsorted adjacent elements`,
     `Iterating  (length -1) times since we need minimum 2 elements to swap`,
     `Inner loop has (length - i - 1) times iteration as for each iteration of outer loop (i), we get largest element at last. Example i= 0, we get largest at last,  so when i= 1, we can skip 1 element`
+  ];
+
+  complexity = [
+    `Worst Case Time Complexity [ Big-O ]: O(n^2)`,
+    `Best Case Time Complexity [Big-omega]: O(n) (Already sorted array)`,
+    `Average Time Complexity [Big-theta]: O(n^2)`,
+    `Space Complexity: O(1) (A single additional memory space is required i.e. for temp)`
   ];
 }
