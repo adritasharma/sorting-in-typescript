81b9db82cc83e8b95c17457b857915b4ff67b6d3
adritasharma <adritasharma.developer@gmail.com>
2020-05-07 02:07:43 +0530

    Passes added in Bubble Sort

--- src/app/bubble-sort/bubble-sort.component.ts
+++ src/app/bubble-sort/bubble-sort.component.ts
@@ -12,19 +12,24 @@ export class BubbleSortComponent implements OnInit {
 
   output = [...this.input];
 
+  passes = [];
+
   ngOnInit() {
     let length = this.output.length;
     let arr = this.output;
 
     for (var i = 0; i < length - 1; i++) {
+      let subPasses = [];
       for (var j = 0; j < length - i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
           var temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
         }
+        subPasses.push([...arr]);
       }
       console.log(arr);
+      this.passes.push(subPasses);
     }
   }
   algorithm = `
