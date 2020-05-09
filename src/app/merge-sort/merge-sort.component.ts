d1b0abf68d50bc15fb274909b6dcb42a7921c806
adritasharma <adritasharma.developer@gmail.com>
2020-05-09 20:24:07 +0530

    1 file modified

--- src/app/merge-sort/merge-sort.component.ts
+++ src/app/merge-sort/merge-sort.component.ts
@@ -9,40 +9,45 @@ export class MergeSortComponent implements OnInit {
   constructor() {}
 
   input = [9, 5, 1, 4, 3];
+  // input = [9];
 
   output = [...this.input];
   passes = [];
 
   ngOnInit() {
-    this.mergeSort([...this.input]);
+    this.mergeSort([...this.input], 0, this.input.length - 1);
   }
 
-  mergeSort(arr) {
-    let length = arr.length;
-    let mid = parseInt(length / 2);
-    console.log("mid", mid);
+  mergeSort(arr, firstIndex, lastIndex) {
+    // let length = arr.length;
+    let midIndex = parseInt((firstIndex + lastIndex) / 2);
+
+    console.log("arr", arr);
+    console.log(firstIndex, lastIndex);
+    console.log("mid", midIndex);
 
     let left = [];
     let right = [];
 
-    for (let i = 0; i < mid; i++) {
+    for (let i = 0; i <= midIndex; i++) {
       left[i] = arr[i];
     }
 
-    for (let i = mid; i < length; i++) {
-      right[i - mid] = arr[i];
+    for (let i = midIndex + 1; i <= lastIndex; i++) {
+      right[i - midIndex - 1] = arr[i];
     }
 
     console.log("left", left);
     console.log("right", right);
     console.log("==========================");
 
-    if (left.length > 1) {
-      this.mergeSort(left);
-    }
+    // if (left.length > 1) {
+    //   this.mergeSort(left);
+    // }
 
-    if (right.length > 1) {
-      this.mergeSort(right);
+    if (firstIndex < lastIndex) {
+      this.mergeSort(left, firstIndex, midIndex);
+      this.mergeSort(right, midIndex + 1, lastIndex);
     }
   }
 }
