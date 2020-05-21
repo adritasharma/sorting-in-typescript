4ef3b380492afc00ff6e74268b1b12ce9a756832
adritasharma <adritasharma.developer@gmail.com>
2020-05-21 20:15:56 +0530

    Binary search logic implemented

--- src/app/search/binary-search/binary-search.component.ts
+++ src/app/search/binary-search/binary-search.component.ts
@@ -11,17 +11,32 @@ export class BinarySearchComponent implements OnInit {
   array = [10, 20, 30, 40, 50];
 
   ngOnInit() {
-    this.BinarySearch(this.array, 20);
+    var pos = this.BinarySearch(this.array, 40);
+    console.log("pos", pos);
   }
 
-  BinarySearch(A:Array<number>, element) {
+  BinarySearch(A: Array<number>, element) {
     let n = A.length;
 
     let start = 0;
-    let end = n-1;
+    let end = n - 1;
 
-    while(start <= end){
-      let mid = (start + end)/2;
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      console.log("---------------------");
+      console.log("start", start);
+      console.log("mid", mid);
+      console.log("end", end);
+
+      if (A[mid] == element) {
+        return mid;
+      } else if( element < A[mid]) {
+	        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
     }
+
+    return -1;
   }
 }
